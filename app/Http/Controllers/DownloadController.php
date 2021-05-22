a0edec7b0c82e0cfff3a09ae46158fb118046a49
karthikmasters@gmail.com <kmarri@americanfirstfinance.com>
2021-05-22 08:05:23 +0530

    Adding dispatchJob method

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Jobs\DownloadUserFiles;
 
 class DownloadController extends Controller
 {
-    //
+    // Dispatch Download Job
+    public function dispatchJob() {
+        $downloadJob = new DownloadUserFiles();
+        dispatch($downloadJob);
+    }
 }
