db0d6abdfeb1d5be6029da1f2fcca40a0e3a6b6f
karthikmasters@gmail.com <kmarri@americanfirstfinance.com>
2021-05-22 08:30:24 +0530

    Passing files list and zip file name parameters to DownloadUserfiles Job

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Jobs\DownloadUserFiles;
+use Auth;
 
 class DownloadController extends Controller
 {
     // Dispatch Download Job
     public function dispatchJob() {
-        $downloadJob = new DownloadUserFiles();
+
+        // Getting file list
+        $aFiles = Auth::user()->getFileList();
+
+        // Setting Directory/Zip file name
+        $sDirectory =  Auth::user()->id . "-" . date('Y-m-d-H-i-s');
+
+        // Passing FileList and Directory parameters
+        $downloadJob = new DownloadUserFiles($aFiles, $sDirectory);
+
+        // Dispatching DownloadJob
         dispatch($downloadJob);
     }
 }
